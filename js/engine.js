5fba777f847a5a886c375ef0d0c2a50af53a3643
Thiago Marques <tm.uea.2014@gmail.com>
2017-02-02 18:00:41 -0400

    Salvando os dados de treinamento no storage do navegador

--- js/engine.js
+++ js/engine.js
@@ -1,6 +1,120 @@
 var debug = true;
+var cursors;
 
-var data = [];
+var storage = (function(){
+    var _data = [];
+    var _index;
+    var _new_data_map, _new_data;
+
+    function recovery(){
+        _data = [];
+        var total = parseInt(localStorage.total_registros);
+        _index = total = (isNaN(total))? 0 : total;
+        for(var i = 0; i<total; i++){
+            var test = localStorage.getItem('test_' + i);
+            _data[i] = test.split(',');
+        }
+
+        return _data;
+    }
+
+    function read(){
+        var text = '';
+        var new_data = shuffle(_data);
+        for(var key in new_data){
+           text += new_data[key].join() + "\n";
+        }
+        console.log(text);
+    }
+
+    function new_data(){
+        _new_data_map = {};
+        return this;
+    }
+
+    function add(label, value){
+        if(!_new_data_map){
+            console.error('O mapa ainda nao foi iniciado { _new_data_map = {} }');
+            return;
+        }
+        _new_data_map[label] = value;
+    }
+
+    function save_new_data(){
+        if(!_user){
+            console.error('Nenhum usuario detectado { ?user=x }');
+            return;
+        }
+
+        var hasEnought = Object.keys(_new_data_map).length;
+
+        if(!hasEnought) return;
+
+        console.log(hasEnought, _new_data_map);
+
+        var ordened = ['rate_blink_left', 'rate_blink_right', 'rate_smile_or_not', 'blink_left', 'blink_right', 'smile_or_not'];
+
+        _new_data = [];
+        
+        _new_data.unshift(_user);
+
+        // Traduz o mapa de valores para um array
+        for(var key in ordened){
+            var value = _new_data_map[ordened[key]];
+            _new_data.push((value)? value : 0);
+        }
+        
+        console.log('Adicionando - ', _new_data);
+        
+        // Adiciona o array dentro da lista
+        _data.push(_new_data);
+        // Incrementa o indice do ultimo salvo
+        _index++;
+    }
+
+    function save(){
+        if(_index <= 0){
+            console.error('Nenhum indice registrado { _index = NaN | 0 }');
+            return;
+        }
+        for(var i = 0; i<_index; i++){
+            var test = _data[i].join();
+            localStorage.setItem('test_' + i, test); 
+        }
+        _new_data = [];
+        _new_data_map = {};
+        localStorage.setItem('total_registros', _index);
+        console.log('Dados armazenados');
+    }
+
+    function check_url_parameter(){
+        console.log(location);
+        var search = location.search.substring(1);
+        var v = search.split('&');
+        for(var i in v){
+            var param = v[i].split('=');
+            var label = param[0];
+            var value = param[1];
+            if(label == 'user'){
+               setUser(value);
+            }
+        }
+    }
+
+    function setUser(value){
+        _user = parseInt(value);
+    }
+
+    return{
+        recovery : recovery,
+        read : read,
+        new_data : new_data,
+        add : add,
+        save_new_data : save_new_data,
+        save : save,
+        check_url_parameter : check_url_parameter
+    }
+})();
 
 /** Objeto principal */
 var body = (function(){
@@ -77,6 +191,8 @@ var body = (function(){
     function smile(value){
         if( typeof value == 'string' )
             value = parseFloat(value.replace(',', '.'));
+
+        storage.add('rate_smile_or_not', value);
         
         if(value < 0.2 && value > 0){
             body.sad();
@@ -95,15 +211,26 @@ var body = (function(){
         if( what_eye == 'left' ){
             _state.blink.right = true;
             _state.blink.left = false;
+            // Salva a taxa em tempo real da probabilidade de olho esta piscando
+            storage.add('rate_blink_left', value);
         }else{
             _state.blink.right = false;
             _state.blink.left = true;
+            // Salva a taxa em tempo real da probabilidade de olho esta piscando
+            storage.add('rate_blink_right', value);
         }
 
-        if(value < 0.5 && value > 0) 
+        // Piscou o olho
+        if(value < 0.5 && value > 0){
             updateFrame(eye_selected, eye_selected.frames.end);
-        else if(value > 0.5){
-            updateFrame(eye_selected, eye_selected.frames.begin);   
+            // Salva a ocorrencia de piscar o olho direito ou esquerdo
+            storage.add('blink_left', (what_eye == 'left')? 1 : 0);
+            storage.add('blink_right', (what_eye == 'right')? 1 : 0);
+        }else if(value > 0.5){ // Nao piscou
+            updateFrame(eye_selected, eye_selected.frames.begin);
+            // Salva a ocorrencia de piscar o olho direito ou esquerdo
+            storage.add('blink_left', (what_eye == 'left')? 0 : 1);
+            storage.add('blink_right', (what_eye == 'right')? 0 : 1);   
         }
 
         //console.log(value);
@@ -112,11 +239,13 @@ var body = (function(){
     function sad(){
         _mouth.anchor.setTo(0.4);
         _mouth.angle = 180;
+        storage.add('smile_or_not', 0);
     }
 
     function happy(){
         _mouth.anchor.setTo(0.6);
         _mouth.angle = 0;
+        storage.add('smile_or_not', 1);
     }
     
     return{
@@ -205,7 +334,7 @@ var server = (function(){
 
     function smile(detection){
         console.log(detection);
-        
+
         body.smile(detection.mouth);
     }
 
@@ -225,8 +354,6 @@ var server = (function(){
         bootstrap : bootstrap
     }
 })();
-
-var game = new Phaser.Game(screen.width, screen.height, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update : update });
 
 function preload() {
 
@@ -250,8 +377,6 @@ function clearGameCache () {
     game.load.removeAll();
 }
 
-var cursors;
-
 function create() {
 
     //console.log(bot);
@@ -273,6 +398,21 @@ function create() {
     tween.mouth();
 
     cursors = game.input.keyboard.createCursorKeys();
+
+     game.input.keyboard.onDownCallback = function() {
+        //console.log(game.input.keyboard.event.keyCode);
+        var code = game.input.keyboard.event.keyCode;
+        switch(code){
+            case 13 : // Enter
+                // Salva todas as ocorrencias
+                storage.save();
+                break;
+            case 77 : // Tecla M
+                // Mostra todos os registros encontrados
+                storage.read();
+                break;
+        }       
+     };
 }
 
 function updateFrame(obj, frame){
@@ -281,6 +421,9 @@ function updateFrame(obj, frame){
 }
 
 function update(){
+
+    // Inicia o processo de armazenamento de eventos
+    storage.new_data();
 
     if (cursors.left.isDown){
         // Pisca olho esquerdo
@@ -299,4 +442,35 @@ function update(){
     }else if (cursors.down.isDown){
         body.happy();
     }
+
+    // Salva a nova ocorrencia de eventos
+    storage.save_new_data();
 }
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// Recupera os dados de testes armazenados
+storage.recovery();
+// Ler e mostra os dados armazenados
+storage.read();
+// Interpreta os parametros passados pela URL
+storage.check_url_parameter();
+// Inicia o game
+var game = new Phaser.Game(screen.width, screen.height, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update : update });
